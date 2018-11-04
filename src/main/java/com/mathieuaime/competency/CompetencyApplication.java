925860d0fb106544bd7076cc296871fca9f11d31
Mathieu Aimé <aime.mathieu1@gmail.com>
2018-11-04 04:32:16 +0100

    Add context load test

--- src/main/java/com/mathieuaime/competency/CompetencyApplication.java
+++ src/main/java/com/mathieuaime/competency/CompetencyApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CompetencyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CompetencyApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(CompetencyApplication.class, args);
+  }
 }
--- src/test/java/com/mathieuaime/competency/CompetencyApplicationTests.java
+++ src/test/java/com/mathieuaime/competency/CompetencyApplicationTests.java
@@ -9,8 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class CompetencyApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Test
+  public void contextLoads() {
+    CompetencyApplication.main(new String[]{});
+  }
 
 }
