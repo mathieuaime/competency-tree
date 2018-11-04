847fbbbcd874d20c404b087553a7baa93f20bbe3
Mathieu Aimé <aime.mathieu1@gmail.com>
2018-11-04 04:14:24 +0100

    Setup Spring boot & update circleci config

---
+++ src/main/java/com/mathieuaime/competency/CompetencyApplication.java
@@ -0,0 +1,12 @@
+package com.mathieuaime.competency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetencyApplication.class, args);
+	}
+}
